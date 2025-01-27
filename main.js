4efbc661499f21f693f6cc082db3571eec892c8d
Peter-Gadelsayed <peter.gadelsayed@outlook.com>
2025-01-27 21:17:20 +0200

    Slider

---
+++ main.js
@@ -0,0 +1,51 @@
+var closeIcon = document.getElementById("closeIcon");
+var nextIcon = document.getElementById("nextIcon");
+var prevIcon = document.getElementById("prevIcon");
+var sliderContainer = document.getElementById("sliderContainer");
+
+var imgList = document.querySelectorAll('.img-container img');
+
+var currentIndex = 0;
+
+
+
+closeIcon.addEventListener("click", closeSlider);
+nextIcon.addEventListener("click", nextSlide);
+prevIcon.addEventListener("click", prevSlide);
+
+for (var i = 0; i < imgList.length; i++) {
+    imgList[i].addEventListener("click", function () {
+        showSlider(i)
+        sliderContainer.style.display = "flex";
+    });
+}
+
+
+function closeSlider() {
+    sliderContainer.style.display = "none";
+}
+
+
+function showSlider(index) {
+    var sliderImg = document.getElementById('sliderImg');
+    sliderImg.style.backgroundImage = `url(${imgList[index].src})`;
+    console.log(imgList[index].src)
+    currentIndex = index;
+}
+
+
+function nextSlide() {
+    currentIndex = currentIndex + 1;
+    if (currentIndex == imgList.length) {
+        currentIndex = 0
+    }
+    showSlider(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = currentIndex - 1;
+    if (currentIndex < 0) {
+        currentIndex = imgList.length - 1
+    }
+    showSlider(currentIndex);
+}
